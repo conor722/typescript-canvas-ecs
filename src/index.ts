6b55dade9ffd27ff1dd2199adbe5940ee30fc570
conor <conormckechnie234@gmail.com>
2024-08-25 18:10:52 +0100

    Get hot reload and image loading working and also conclude I hate webpack

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import { Position, Render, Velocity } from './systems';
+import Pic from '../images/man.jpg';
+
+const body = document.body;
+const canvas = document.createElement('canvas');
+canvas.height = 150;
+canvas.width = 150;
+canvas.id = 'tutorial';
+body.appendChild(canvas);
 
 const entity = 1;
 
 Position.register(entity, { x: 0, y: 0, z: 0 });
-Render.register(entity, 'images/man.jpg');
+Render.register(entity, Pic);
 Velocity.register(entity, { x: 0.3, y: 0, z: 0 });
 
 Render.processComponents(1);
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.ts',
@@ -9,6 +10,9 @@ module.exports = {
                 test: /\.ts$/,
                 use: 'ts-loader',
                 exclude: /node_modules/,
+            }, {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
             },
         ],
     },
@@ -19,4 +23,9 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Custom template",
+        }),
+    ]
 };
